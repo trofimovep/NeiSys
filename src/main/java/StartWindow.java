014b5a6176b59cae29d44bd156e98d385b34e3d0
Eugene <trofimovep@list.ru>
2017-05-26 18:22:57 +0300

    draw Circules with color

--- src/main/java/StartWindow.java
+++ src/main/java/StartWindow.java
@@ -1,14 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-public class StartWindow extends JFrame{
+public class StartWindow extends JFrame {
 
     Toolkit tk = Toolkit.getDefaultToolkit();
     int xsize = (int) tk.getScreenSize().getWidth();
     int ysize = (int) tk.getScreenSize().getHeight();
-
 
 
     public StartWindow(String s) {
@@ -16,44 +17,40 @@ public class StartWindow extends JFrame{
 
 
         /// draw Knots Panel
-
         final JPanel drawPanel = new JPanel();
         drawPanel.setLayout(null);
         setLayout(new GridLayout(2, 1));
         drawPanel.setBackground(Color.DARK_GRAY);
         add(drawPanel);
+        drawPanel.addMouseListener(new MouseListener() {
+                                       public void mouseClicked(MouseEvent mouseEvent) {
+                                           int x = mouseEvent.getX();
+                                           int y = mouseEvent.getY();
+                                           Graphics gr = drawPanel.getGraphics();
+                                           System.out.println(x);
+                                           
+                                           gr.setColor(Color.YELLOW);
+                                           gr.fillOval(x, y, 20, 20);
 
-        drawPanel.addMouseListener(new MouseListener(){
-            public void mouseClicked(MouseEvent mouseEvent) {
-                int x = getX();
-                int y = getY();
-                Graphics g = null;
-                g.drawOval(x, y, 20, 20);
+//                                           drawPanel.add(paintComponents(g, x, y));
+                                       }
 
+                                       public void mousePressed(MouseEvent mouseEvent) {
 
-            }
+                                       }
 
-            public void mousePressed(MouseEvent mouseEvent) {
+                                       public void mouseReleased(MouseEvent mouseEvent) {
 
-            }
+                                       }
 
-            public void mouseReleased(MouseEvent mouseEvent) {
+                                       public void mouseEntered(MouseEvent mouseEvent) {
 
-            }
+                                       }
 
-            public void mouseEntered(MouseEvent mouseEvent) {
+                                       public void mouseExited(MouseEvent mouseEvent) {
 
-            }
-
-            public void mouseExited(MouseEvent mouseEvent) {
-
-            }
-        });
-
-
-
-
-
+                                       }
+                                   });
 
 
 
@@ -67,8 +64,20 @@ public class StartWindow extends JFrame{
         jj.setVisible(true);
         buttonsPanel.add(jj);
 
+
+
+//        public void paintComponents(Graphics g){
+//            super.paintComponents(g);
+//
+//            g.drawOval(x, y, 20, 20);
+//            g.setColor(Color.green);
+//        }
+
+
+
     }//StartWindow
 
+}//class
 
 
 
@@ -76,15 +85,19 @@ public class StartWindow extends JFrame{
 
 
 
-//public void drawKnots(Graphics g, int x, int y){
-//        super.drawKnots(g);
-//g.drawOval(x, y, 20, 20);
-//g.setColor(Color.green);
-//
-//}//drawKnots
 
 
 
 
 
-}
+
+
+
+
+
+
+
+//class
+
+
+
