44caa2446a92d53a0582f5298dcb3f2784d600c8
Trofimov Eugene <trofimovep@list.ru>
2017-06-22 13:16:37 +0400

    Merge branch 'master' of C:\Users\nash\IdeaProjects\NeiSys with conflicts.

--- src/main/java/Knot.java
+++ src/main/java/Knot.java
@@ -1,12 +1,24 @@
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
 /**
  * Created by nash on 5/26/17.
  */
-public  class Knot{
+public abstract class Knot{
 
-//public Knot(boolean type, int id, int x, int y){
+    boolean type; // тип узла: состояние(true) или управление (false)
+    int x;      // координата x
+    int y;     // координата y
 
+     protected static Ellipse2D getView(Graphics2D g, int x, int y) {
+        Ellipse2D view = new Ellipse2D.Double(x - 40 / 2 ,y - 40 / 2, 40, 40);
+        return view;
+    }
 
-public boolean[] ties; //матрица инцинденции ??
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public boolean isType() { return type; }
+
 
 
 }
--- src/main/java/StartWindow.java
+++ src/main/java/StartWindow.java
@@ -4,79 +4,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+//import java.awt.geom.Ellipse2D;
 import java.util.ArrayList;
 
 public class StartWindow extends JFrame {
 
-    Toolkit tk = Toolkit.getDefaultToolkit();
-    int xsize = (int) tk.getScreenSize().getWidth();
-    int ysize = (int) tk.getScreenSize().getHeight();
+    ButtonGroup type_knot = new ButtonGroup();
+    JRadioButton state_knot = new JRadioButton("Узел состояния");
+    JRadioButton control_knot = new JRadioButton("Узел управления");
 
+    Toolkit tk = Toolkit.getDefaultToolkit();
+//    int xsize = (int) tk.getScreenSize().getWidth();
+//    int ysize = (int) tk.getScreenSize().getHeight();
 
     public StartWindow(String s) {
         super(s);
-        /// draw Knots Panel
+        eHandler handler = new eHandler();
+        final ArrayList<State> States = new ArrayList<State>();
+
+//        / draw Knots Panel
         final JPanel drawPanel = new JPanel();
         drawPanel.setLayout(null);
         setLayout(new GridLayout(2, 1));
         drawPanel.setBackground(Color.DARK_GRAY);
         add(drawPanel);
-
-        final ArrayList<State> States = new ArrayList<State>();
-
-        drawPanel.addMouseListener(new MouseListener() {
-
-            int clicks = 0;
-            public void mouseClicked(MouseEvent mouseEvent) {
-
-                int x = mouseEvent.getX();
-                int y = mouseEvent.getY();
-                Graphics gr = drawPanel.getGraphics();
-                Graphics2D gr2d = (Graphics2D) gr;
-                clicks++;
-
-                boolean type = true;
-                // во не помню, добавлял ли я эту строчку, или из-за нее
-                States.add(new State(type, clicks, x, y));
-                int current = States.size() - 1;
-                for (int i = 0; i < States.size() - 1; i++) {
-
-                    if (Math.abs(States.get(i).view.getCenterX() - States.get(current).view.getCenterX()) < 20 &&
-                            Math.abs(States.get(i).view.getCenterY() - States.get(current).view.getCenterY()) < 20
-                            ) {
-                        States.remove(current);
-                        break;
-                    }
-                }
-                gr2d.setColor(Color.YELLOW);
-                for (int i = 0; i < States.size(); i++)
-                    gr2d.fill(States.get(i).getView());
-
-            }
-
-
-               public void mousePressed(MouseEvent mouseEvent) {
-
-               }
-
-               public void mouseReleased(MouseEvent mouseEvent) {
-
-               }
-
-               public void mouseEntered(MouseEvent mouseEvent) {
-
-               }
-
-               public void mouseExited(MouseEvent mouseEvent) {
-
-               }
-           });
-
-
-
         // low panel with buttons
-
-
         JPanel buttonsPanel = new JPanel();
         buttonsPanel.setLayout(null);
         add(buttonsPanel);
@@ -87,37 +39,80 @@ public class StartWindow extends JFrame {
         jj.setVisible(true);
         buttonsPanel.add(jj);
 
-        ButtonGroup type_knot = new ButtonGroup();
-        JRadioButton state_knot = new JRadioButton("Узел состояния");
-        state_knot.setBounds(20, 220, 100, 20);
+        control_knot.setBounds(20, 240, 200, 20);
+        state_knot.setBounds(20, 220, 200, 20);
+        type_knot.add(control_knot);
         type_knot.add(state_knot);
         buttonsPanel.add(state_knot);
+        buttonsPanel.add(control_knot);
+        state_knot.addActionListener(handler);
+        control_knot.addActionListener(handler);
+        boolean type; //тип узла: состояние(true) или управление (false)
 
+        drawPanel.addMouseListener(new MouseListener() {
+            int clicks = 0;
+            public void mouseClicked(MouseEvent mouseEvent) {
+                int x = mouseEvent.getX();
+                int y = mouseEvent.getY();
+                Graphics gr = drawPanel.getGraphics();
+                Graphics2D gr2d = (Graphics2D) gr;
+                clicks++;
 
+                States.add(new State(true, clicks, x, y));
+                System.out.println("Size = " + States.size());
+                int current = States.size() - 1;
+                for (int i = 0; i < States.size() - 1; i++) {
+
+                    if (Math.abs(States.get(i).getX() - States.get(current).getX()) < 50 &&
+                            Math.abs(States.get(i).getY() - States.get(current).getY()) < 50) {
+                        System.out.println("Size before = " + States.size());
+                       States.remove(current);
+                        System.out.println("Size after = " + States.size());
+                         break;
+                    }
+//
+                }
+                gr2d.setColor(Color.ORANGE);
+//                for (State State : States) gr2d.fill(State.getView(gr2d, x, y));
+                gr2d.fill(State.getView(gr2d, x, y));
+            }
+               public void mousePressed(MouseEvent mouseEvent) { }
+               public void mouseReleased(MouseEvent mouseEvent) {
+
+               }
+               public void mouseEntered(MouseEvent mouseEvent) {
+
+               }
+               public void mouseExited(MouseEvent mouseEvent) {
+
+               }
+           });
 
     }//StartWindow
+
+    class eHandler implements ActionListener{
+        boolean type; // тип узла : состояние(true) или управление(false)
+
+        public void actionPerformed(ActionEvent e) {
+            try{
+                if(e.getSource() ==  state_knot){
+                type = true;
+                }
+                if(e.getSource() == control_knot){
+                JOptionPane.showMessageDialog(null, "Control knot");
+                }
+            }
+            catch (Exception ex){
+
+            }
+
+        }
+    }
 
 }//class
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//class
 
 
 
