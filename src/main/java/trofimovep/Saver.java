ccb4d8944299b0688ed7f4686ef13031bb9c9f2d
johnnash <trofimovep.mt@gmail.com>
2017-11-24 07:33:19 +0300

    use for fun

--- src/main/java/trofimovep/Saver.java
+++ src/main/java/trofimovep/Saver.java
@@ -14,7 +14,7 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-import static com.sun.javafx.fxml.expression.Expression.add;
+//import static com.sun.javafx.fxml.expression.Expression.add;
 
 //import static sun.plugin2.util.PojoUtil.toJson;
 
