4caf4431e5838e86f08abe783fb75b46d2302368
johnnash <trofimovep.mt@gmail.com>
2017-11-24 08:08:08 +0300

    use for fun

---
+++ src/main/java/trofimovep/KnotsSorter.java
@@ -0,0 +1,18 @@
+package trofimovep;
+
+import java.util.ArrayList;
+
+public class KnotsSorter {
+
+    protected static void lastKnot(ArrayList<Knot> knots){
+        ArrayList<State> states = Identificater.getStates(knots);
+
+        for (State st : states) {
+
+        }
+
+    }
+
+
+
+}
